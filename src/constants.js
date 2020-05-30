ae2a622fa4ab0f17a6a4d9abc76802054839c2ab
ajayvarghese <ajay.varghese@freighthub.com>
2020-05-30 16:05:55 +0200

    Server Change

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const ENDPOINT = "https://i-general-purpose-server.herokuapp.com/";
+const ENDPOINT = "https://i-general-purpose-server.herokuapp.com";
 // const ENDPOINT = "http://localhost:3000";
 export const URLS = {
   FOREX: `${ENDPOINT}/eur-to-inr`,
